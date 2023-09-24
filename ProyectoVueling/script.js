5066f311d9da5e56bfd875597287660021229dbb
Shah Zaib <shebiraaja@gmail.com>
2023-09-24 21:33:17 +0200

    changed visibility of forms

--- ProyectoVueling/script.js
+++ ProyectoVueling/script.js
@@ -1,39 +1,52 @@
 
 //funcion para hacer aparecer y desaparecer el formulario de login
 document.getElementById("login-btn").addEventListener("click", () => {
+
+    document.getElementById("login-div").style.display = "block";
+    document.getElementById("register-div").style.display = "none";
+    document.getElementById("booking-div").style.display = "none";
     
 
-    if(document.getElementById("login-div").style.display == "block") {
-        // console.log("se ve")
-        document.getElementById("login-div").style.display = "none";
-    }else {
-        // console.log("no se ve")
-        document.getElementById("login-div").style.display = "block";
-    }
+    // if(document.getElementById("login-div").style.display == "block") {
+    //     // console.log("se ve")
+    //     document.getElementById("login-div").style.display = "none";
+    // }else {
+    //     // console.log("no se ve")
+    //     document.getElementById("login-div").style.display = "block";
+    // }
 })
 
 //funcion para hacer aparecer y desaparecer el formulario de register
 document.getElementById("register-btn").addEventListener("click", () => {
+
+    document.getElementById("login-div").style.display = "none";
+    document.getElementById("register-div").style.display = "block";
+    document.getElementById("booking-div").style.display = "none";
     
-    if(document.getElementById("register-div").style.display == "block") {
-        // console.log("se ve")
-        document.getElementById("register-div").style.display = "none";
-    }else {
-        // console.log("no se ve")
-        document.getElementById("register-div").style.display = "block";
-    }
+    // if(document.getElementById("register-div").style.display == "block") {
+    //     // console.log("se ve")
+    //     document.getElementById("register-div").style.display = "none";
+    // }else {
+    //     // console.log("no se ve")
+    //     document.getElementById("register-div").style.display = "block";
+    // }
 })
 
 //funcion para hacer aparecer y desaparecer el formulario de booking
 document.getElementById("booking-btn").addEventListener("click", () => {
+
+
+    document.getElementById("login-div").style.display = "none";
+    document.getElementById("register-div").style.display = "none";
+    document.getElementById("booking-div").style.display = "block";
     
-    if(document.getElementById("booking-div").style.display == "block") {
-        // console.log("se ve")
-        document.getElementById("booking-div").style.display = "none";
-    }else {
-        // console.log("no se ve")
-        document.getElementById("booking-div").style.display = "block";
-    }
+    // if(document.getElementById("booking-div").style.display == "block") {
+    //     // console.log("se ve")
+    //     document.getElementById("booking-div").style.display = "none";
+    // }else {
+    //     // console.log("no se ve")
+    //     document.getElementById("booking-div").style.display = "block";
+    // }
 })
 
 
