eb453e0f0c06762b02988253e07a8b78efb3d3be
shebiraaja <shebiraaja@gmail.com>
2023-09-27 10:21:25 +0200

    actualizado CSS y JS

--- ProyectoVueling/script.js
+++ ProyectoVueling/script.js
@@ -1,11 +1,22 @@
+//cuando cargue la página todos los forms estarán en display none, (no se verán)
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("login-div").style.display = "none";
+    document.getElementById("register-div").style.display = "none";
+    document.getElementById("booking-div").style.display = "none";
+});
 
-//funcion para hacer aparecer y desaparecer el formulario de login
+
+//funcion para hacer aparecer el formulario de login
 document.getElementById("login-btn").addEventListener("click", () => {
 
     document.getElementById("login-div").style.display = "block";
     document.getElementById("register-div").style.display = "none";
     document.getElementById("booking-div").style.display = "none";
     
+
+
+    /************************************************************************** */
+
 
     // if(document.getElementById("login-div").style.display == "block") {
     //     // console.log("se ve")
@@ -14,15 +25,21 @@ document.getElementById("login-btn").addEventListener("click", () => {
     //     // console.log("no se ve")
     //     document.getElementById("login-div").style.display = "block";
     // }
-})
+});
 
-//funcion para hacer aparecer y desaparecer el formulario de register
+//funcion para hacer aparecer el formulario de register
 document.getElementById("register-btn").addEventListener("click", () => {
 
     document.getElementById("login-div").style.display = "none";
     document.getElementById("register-div").style.display = "block";
     document.getElementById("booking-div").style.display = "none";
+
+
+
+    /************************************************************************** */
+
     
+
     // if(document.getElementById("register-div").style.display == "block") {
     //     // console.log("se ve")
     //     document.getElementById("register-div").style.display = "none";
@@ -30,15 +47,22 @@ document.getElementById("register-btn").addEventListener("click", () => {
     //     // console.log("no se ve")
     //     document.getElementById("register-div").style.display = "block";
     // }
-})
+});
 
-//funcion para hacer aparecer y desaparecer el formulario de booking
+//funcion para hacer aparecer el formulario de booking
 document.getElementById("booking-btn").addEventListener("click", () => {
 
 
     document.getElementById("login-div").style.display = "none";
     document.getElementById("register-div").style.display = "none";
     document.getElementById("booking-div").style.display = "block";
+
+
+    
+    
+    /************************************************************************** */
+
+
     
     // if(document.getElementById("booking-div").style.display == "block") {
     //     // console.log("se ve")
@@ -47,7 +71,7 @@ document.getElementById("booking-btn").addEventListener("click", () => {
     //     // console.log("no se ve")
     //     document.getElementById("booking-div").style.display = "block";
     // }
-})
+});
 
 
 //Hacer un get de los datos introducidos en los fields del formulario login, y después mostrarlos en un alert.
@@ -59,4 +83,7 @@ document.getElementById("loginNowBtn").addEventListener("click", () => {
 
     alert("You email is " + email + " and your password is " + passwd)
 
-})
+});
+
+
+
