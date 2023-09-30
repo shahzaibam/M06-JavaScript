8f5ec901ddaa6173fcc05c13453887fa4acab6b7
Shah Zaib <shebiraaja@gmail.com>
2023-10-01 01:21:46 +0200

    made the login form

--- ProyectoVueling/vendor/js/script.js
+++ ProyectoVueling/vendor/js/script.js
@@ -1,11 +1,15 @@
+//usuario
+let usuarios = "daw2@proven.cat";
+let passwd = "alumnat";
+
 //cuando cargue la página todos los forms estarán en display none, (no se verán)
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("login-div").style.display = "none";
     document.getElementById("register-div").style.display = "none";
     document.getElementById("booking-div").style.display = "none";
     document.getElementById("result").style.display = "none";
+    document.getElementById("validationMessage").style.display = "none";
 });
-
 
 //funcion para hacer aparecer el formulario de login
 document.getElementById("login-btn").addEventListener("click", () => {
@@ -45,19 +49,29 @@ document.getElementById("booking-btn").addEventListener("click", () => {
 //Hacer un get de los datos introducidos en los fields del formulario login, y después mostrarlos en un alert.
 document.getElementById("loginNowBtn").addEventListener("click", () => {
     
-    let email = document.getElementById("email").value;
+    let emailVal = document.getElementById("email").value;
 
-    let passwd = document.getElementById("password").value;
+    let passwdVal = document.getElementById("password").value;
+
+    if(emailVal == usuarios && passwdVal == passwd) {
+        document.getElementById("validationMessage").innerHTML = "Usuari Correcte";
+        document.getElementById("validationMessage").style.color = "green";
+        document.getElementById("validationMessage").style.display = "block";
+    }else {
+        document.getElementById("validationMessage").innerHTML = "Credencials incorrectes";
+        document.getElementById("validationMessage").style.color = "red";
+        document.getElementById("validationMessage").style.display = "block";
+    }
 
     document.getElementById("result").style.display = "block";
 
 
 
-    document.getElementById("result").innerHTML = "<h2 class='text-center pt-3'>Login Results </h2> <p class='pt-4'>Email : " + email + " </p> </p> Password : " + passwd + " </p>";
+    document.getElementById("result").innerHTML = "<h2 class='text-center pt-3'>Login Results </h2> <p class='pt-4'>Email : " + emailVal     + " </p> </p> Password : " + passwdVal + " </p>";
 
-    alert("Email : " + email + "-----" + "Password + :" + passwd)
 
 });
+
 
 
 
