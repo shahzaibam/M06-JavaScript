e7cf06b47b2dc9f23f5e5b786c3397318bf2e156
Shah Zaib <shebiraaja@gmail.com>
2023-10-16 10:34:55 +0200

    actualizado ejercicio de arrays, añadir valor segun valor de select, si es final añadir al final, si es inici añadir al inici

--- UF1/Practica30porciento/ex2/JS/script.js
+++ UF1/Practica30porciento/ex2/JS/script.js
@@ -1,21 +1,36 @@
 let nomExercici2 = [];
 
-let posicioInici = document.getElementById("optionsAfegir").value;
-
-let posicioFinal = document.getElementById("finalOption").value;
-
-//valida lo que hay en el input, si es mayor de 3 lo añade y si son numeros, si son palabras no lo añade
+//valida lo que hay en el input, si es mayor de 3 y es un numero, lo añade, si son palabras no lo añade, si el valor del select es Inici lo añade al inicio con el metodo unshift(), si el valor del select es final añade el valor al final del array con el metodo push().
 document.getElementById("inputValue").addEventListener("blur", () => {
     let inputValue = document.getElementById("inputValue").value;
 
+    let insertarPlace;
 
-    if(!isNaN(inputValue)) {
+    if (document.getElementById("optionsAfegir").value == "Inici") {
+        insertarPlace = "Inici";
+    }else {
+        insertarPlace = "Final";
+    }
+
+    if(!isNaN(inputValue)) { 
         if(inputValue.length>=3) {
-            nomExercici2.push(inputValue);
 
-            document.getElementById("arrayValorsNeg").innerHTML = inputValue + " valor añadido";
+            if(insertarPlace == "Inici") {
 
-            console.log(nomExercici2);
+                nomExercici2.unshift(inputValue);
+
+                document.getElementById("arrayValorsNeg").innerHTML = inputValue + " valor añadido";
+    
+                console.log(nomExercici2);
+            }else {
+
+                nomExercici2.push(inputValue);
+
+                document.getElementById("arrayValorsNeg").innerHTML = inputValue + " valor añadido";
+    
+                console.log(nomExercici2);
+            }
+
         }else{
             console.log(inputValue);
             document.getElementById("arrayValorsNeg").innerHTML += "Valor negativo no cumple "+  inputValue + "<br>";
