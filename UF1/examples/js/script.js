94b6287a2331640dccc3e39f525f7f4de0c83627
Shah Zaib <shebiraaja@gmail.com>
2023-10-18 11:18:57 +0200

    javascript practice done (new examples)

--- UF1/examples/js/script.js
+++ UF1/examples/js/script.js
@@ -16,6 +16,39 @@ for (let index = 0; index < ciutats.length; index++) {
 }
 
 
+document.getElementById("myBtn").addEventListener("click", () => {
+
+    //fills del select en format array
+    console.log(document.getElementById("myCities").children); //em dona tots els fills del select
+    console.log(document.getElementById("myCities").childNodes);
+
+    //entrem dins del arrays
+    console.log(document.getElementById("myCities").children[0].value);
+    console.log(document.getElementById("myCities").childNodes[1].value);
+
+
+    //agafem el primer i el darrer element
+    console.log(document.getElementById("myCities").firstChild);
+    console.log(document.getElementById("myCities").lastChild.value);
+
+    console.log(document.getElementById("myCities").firstElementChild);
+    console.log(document.getElementById("myCities").lastElementChild.value);
+
+
+    //padre del select
+    console.log(document.getElementById("myCities").parentNode);
+    console.log(document.getElementById("myCities").parentElement);
+
+
+    //esborra un node que sigui child de body i que no estigui en cap div si no que estigui directament en el tag body
+    document.body.removeChild(document.getElementById("paragraf1"));
+
+    //esborrar un node assenyalat amb el id
+    document.getElementById("paragraf2").remove();
+
+})
+
+
 // element.innerHTML= "Texte des de javascript";
 
 
---
+++ UF1/exemplesClasses/js/classes/User.js
@@ -0,0 +1,14 @@
+export class User { //EXPORT = fer visible la classe desde fora
+    
+    //atributs, propietats
+
+
+    //constructor
+
+
+    //getter, setters : recollir un valor d'un atributs o donar el valor
+
+
+    //mètodes : accions de l'usuari
+
+}
