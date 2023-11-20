a837ff7fd0a4c9e08c1c763c6436f0ef83a7daba
Shah Zaib <shebiraaja@gmail.com>
2023-11-20 11:29:16 +0100

    started JQUERY

---
+++ UF2/JQUERY/ejemplo1/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // alert("Hola Jquery!!!");
+
+    //esconde => style.display="none"
+    // $("p").hide(); //accedo a todos los tags p y los escondo
+
+    // $("#myP2").hide();
+    // $(".test").hide();
+    $("p:first").hide();
+
+
+})
+
+
+
+
+
