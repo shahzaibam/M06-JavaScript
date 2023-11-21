2114ef6f6026f41e2bfd84c41fa36a7854efc773
Shah Zaib <shebiraaja@gmail.com>
2023-11-21 22:07:03 +0100

    ejercicio de JQUERY hecho

--- UF2/JQUERY/ejercicio1-form-Jquery/js/index.js
+++ UF2/JQUERY/ejercicio1-form-Jquery/js/index.js
@@ -1,17 +1,30 @@
-$("#mySubmit").click(validarDatos);
-
-function validarDatos() {
+$("#mySubmit").click(() => {
     let nombreVal = $("#nombre").val();
     let DNIVal = $("#dni").val();
     let edadVal = $("#edad").val();
 
+    let valores = validarDatos(nombreVal, DNIVal, edadVal);
+
+    
+    
+    if(valores) {
+        const mensaje = `Nombre: ${nombreVal}, DNI: ${DNIVal}, Edad: ${edadVal}`;
+        $('#datos').text(mensaje).css('color', 'green');
+    }
+    
+});
+
+function validarDatos(nombreVal, DNIVal,edadVal ) {
+
     let okNombre = false;
-    let okDni = false;
+    let okDni = false; 
     let okEdad = false;
 
     var patronNombre = /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/;
-    var patronDNI = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]$/;
+    var patronDNI = /^[0-9]{8}[TtRrWwAaGgMmYyFfPpDdXxBbNnJjZzSsQqVvHhLlCcKkEeTt]$/;
     var patronEdad = /^\d+$/;
+
+    let allGood;
 
 
     if (patronNombre.test(nombreVal)) {
@@ -27,7 +40,6 @@ function validarDatos() {
     if (patronDNI.test(DNIVal)) {
         okDni = true;
         $('#mensajeDni').hide();
-
     } else {
         $('#mensajeDni').show();
         $('#mensajeDni').text('DNI inválido').css('color', 'red');
@@ -35,14 +47,19 @@ function validarDatos() {
 
 
     if (patronEdad.test(edadVal)) {
-        $('#mensajeDni').hide();
+        $('#mensajeEdad').hide();
         okEdad = true;
     } else {
-        $('#mensajeDni').show();
+        $('#mensajeEdad').show();
         $('#mensajeEdad').text('Edad inválida').css('color', 'red');
     }
 
 
+    if(okNombre && okEdad && okDni) {
+        allGood = true;
+    }
+
+    return allGood;
 
 }
 
