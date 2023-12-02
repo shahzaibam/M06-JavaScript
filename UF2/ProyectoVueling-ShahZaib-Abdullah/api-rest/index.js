c5d29306c9907352563750779bf529535aacdf33
Shah Zaib <shebiraaja@gmail.com>
2023-12-02 01:54:33 +0100

    linking the registration form with the database

--- UF2/ProyectoVueling-ShahZaib-Abdullah/api-rest/index.js
+++ UF2/ProyectoVueling-ShahZaib-Abdullah/api-rest/index.js
@@ -64,25 +64,58 @@ function rutas() {
 
 
 
-// GET
-app.get('/vueling/booking', function (req, res) {
-    console.log("Estamos en login GET");
-    connection.query('SELECT nomCiutat FROM ciutats', function (error, results, field) {
-        if (error) {
-            res.status(400).json({
-                error: true,
-                results: null,
-                message: "Error en la consulta a la base de datos"
-            });
-        } else {
-            res.status(200).json({
-                error: false,
-                results: results,
-                message: "Consulta GET HECHA"
-            });
-        }
+    // GET
+    app.get('/vueling/booking', function (req, res) {
+        console.log("Estamos en login GET");
+        connection.query('SELECT nomCiutat FROM ciutats', function (error, results, field) {
+            if (error) {
+                res.status(400).json({
+                    error: true,
+                    results: null,
+                    message: "Error en la consulta a la base de datos"
+                });
+            } else {
+                res.status(200).json({
+                    error: false,
+                    results: results,
+                    message: "Consulta GET HECHA"
+                });
+            }
+        });
     });
-});
+
+
+    // POST route for registration
+    app.post('/vueling/registration', function (req, res) {
+        console.log("Estamos en registro POST");
+
+        // Retrieve registration data from the request body
+        const nom = req.body.nom;
+        const cognoms = req.body.cognoms;
+        const email = req.body.email;
+        const password = req.body.password;
+        const dni = req.body.dni;
+
+        // Insert registration data into the database
+        connection.query('INSERT INTO usuarios (nom, cognoms, email, password, dni) VALUES (?, ?, ?, ?, ?)',
+            [nom, cognoms, email, password, dni],
+            function (error, results, fields) {
+                if (error) {
+                    res.status(400).json({
+                        error: true,
+                        results: null,
+                        message: "Error en la inserción en la base de datos"
+                    });
+                } else {
+                    res.status(200).json({
+                        error: false,
+                        results: results,
+                        message: "Inserción en la base de datos exitosa"
+                    });
+                }
+            }
+        );
+    });
 
 
     app.listen(3000, () => {
