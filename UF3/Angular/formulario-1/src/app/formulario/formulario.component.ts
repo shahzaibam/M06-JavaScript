c5b7080dc93350981cca48d09258215453230cdd
Shah Zaib <shebiraaja@gmail.com>
2023-12-12 23:17:06 +0100

    Formulario reactivo done

--- UF3/Angular/formulario-1/src/app/formulario/formulario.component.ts
+++ UF3/Angular/formulario-1/src/app/formulario/formulario.component.ts
@@ -9,49 +9,34 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class FormularioComponent {
 
-  formulario: FormGroup; //Declaración de una propiedad llamada formulario que representa el formulario reactivo.
-
-  constructor(private fb: FormBuilder) {
-    this.formulario = this.fb.group({
-      nomUsuari: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]{6,}$/)]],
-      contrasenya: ['', [Validators.required, Validators.pattern(/^(?=.*[a-zA-Z])(?=.*\d).{8,}$/)]],
-      confirmarContrasenya: ['', [Validators.required]],
-      correuElectronic: ['', [Validators.required, Validators.email]],
-      estatCivil: ['', [Validators.required]],
-      sexe: ['', [Validators.required]],
-      informacio: this.fb.array([]),
-      acceptarCondicions: [false, [Validators.requiredTrue]]
-    }, { validators: this.passwordMatchValidator });
+  constructor(private readonly fb: FormBuilder) {
   }
 
-  passwordMatchValidator(formGroup: FormGroup) {
-    const passwordControl = formGroup.get('contrasenya');
-    const confirmPasswordControl = formGroup.get('confirmarContrasenya');
+  formulari !: FormGroup; //! --> Al utilizar !, significa que estás seguro de que la variable o propiedad tendrá un valor cuando se acceda a ella,
+  // y que no se necesita verificar explícitamente si es nula o indefinida.
 
-    if (passwordControl && confirmPasswordControl) {
-      const password = passwordControl.value;
-      const confirmPassword = confirmPasswordControl.value;
 
-      if (password !== confirmPassword) {
-        confirmPasswordControl.setErrors({ noCoincide: true });
-      } else {
-        confirmPasswordControl.setErrors(null);
-      }
-    }
+  ngOnInit(): void {
+    this.formulari = this.initForm();
   }
 
-
-  getError(controlName: string, errorType: string): boolean {
-    const control = this.formulario.get(controlName);
-    return control ? control.touched && control.errors && control.errors[errorType] : false;
+  onSubmit():void {
+    console.log(this.formulari.value)
   }
 
-  enviarFormulario() {
-    if (this.formulario.valid) {
-      console.log('Formulario válido. Valores enviados:', this.formulario.value);
-    } else {
-      console.log('Formulario inválido. Por favor, revisa los campos.');
-    }
+  initForm(): FormGroup {
+    return this.fb.group({
+      nomUsuari : ["", [Validators.required, Validators.pattern(/^[a-zA-Z]{6,}$/)]],
+      contrasenya : ["", [Validators.required, Validators.pattern(/^(?=.*[a-zA-Z])(?=.*\d).{8,}$/)]],
+      confirmarContrasenya : ["", [Validators.required]],
+      correuElectronic : ["", [Validators.required, Validators.email]],
+      estatCivil : ["", [Validators.required]],
+      sexe : ["", [Validators.required]],
+      informacio : ["", [Validators.required]],
+      acceptarCondicions : ["", [Validators.required]]
+
+    })
   }
+
 
 }
