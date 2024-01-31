b6faa68068b0a5ce1713427b3a3733627c5a5ef6
Shah Zaib <shebiraaja@gmail.com>
2024-01-31 10:46:23 +0100

    comenzado landing page web upc

--- UF3/Angular/webUPC/src/app/app.module.ts
+++ UF3/Angular/webUPC/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import {HomeComponent} from "./components/home/home.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- UF3/Angular/webUPC/src/app/components/menu/menu.component.ts
+++ UF3/Angular/webUPC/src/app/components/menu/menu.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-  title = "Escribiendo en vivo";
-  currentTitle = '';
 
-  ngOnInit() {
-    this.typeTitle();
-  }
-
-  typeTitle() {
-    let index = 0;
-    const intervalId = setInterval(() => {
-      this.currentTitle += this.title[index];
-      index++;
-      if (index === this.title.length) {
-        clearInterval(intervalId);
-      }
-    }, 100); // ajusta la velocidad de escritura aquí (en milisegundos)
-  }
 }
