46e468f583db4091e54b3fdbdab0f8885eb741a4
Shah Zaib <shebiraaja@gmail.com>
2024-01-31 20:10:31 +0100

    cookies added and pagination

---
+++ UF3/Angular/paginationExample/src/app/components/list-things/list-things.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-things',
+  templateUrl: './list-things.component.html',
+  styleUrls: ['./list-things.component.css']
+})
+export class ListThingsComponent {
+  allThings: any[] = [
+    { name: 'Thing 1' },
+    { name: 'Thing 2' },
+    { name: 'Thing 3' },
+    { name: 'Thing 4' },
+    { name: 'Thing 5' },
+    { name: 'Thing 6' },
+    { name: 'Thing 7' },
+    { name: 'Thing 8' },
+    { name: 'Thing 9' },
+    { name: 'Thing 10' },
+    { name: 'Thing 11' },
+    { name: 'Thing 12' },
+    { name: 'Thing 13' },
+    { name: 'Thing 14' },
+    { name: 'Thing 15' },
+    { name: 'Thing 1' },
+    { name: 'Thing 2' },
+    { name: 'Thing 3' },
+    { name: 'Thing 4' },
+    { name: 'Thing 5' },
+    { name: 'Thing 6' },
+    { name: 'Thing 7' },
+    { name: 'Thing 8' },
+    { name: 'Thing 9' },
+    { name: 'Thing 10' },
+    { name: 'Thing 11' },
+    { name: 'Thing 12' },
+    { name: 'Thing 13' },
+    { name: 'Thing 14' },
+    { name: 'Thing 15' }
+    // Puedes agregar más elementos si lo deseas
+  ];
+
+  // Propiedades para la paginación
+  currentPage = 1;
+  itemsPerPage = 5; // Cantidad de elementos por página
+}
