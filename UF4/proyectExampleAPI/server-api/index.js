150e8aad637883e6f6a4bc06b1614d38a7b43978
Shah Zaib <shebiraaja@gmail.com>
2024-02-07 10:31:19 +0100

    api enviar datos con postman en POST y practicado el GET

--- UF4/proyectExampleAPI/server-api/index.js
+++ UF4/proyectExampleAPI/server-api/index.js
@@ -1,34 +1,87 @@
 'use strict';
 
-//importacions i creació de constants per a la seva utiliutzació
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const cors = require('cors');
 const mysql = require('mysql');
 
 const app = express();
-app.use(cors); //todos los accesos estan 'protegidos' del error de CORS
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'test',
-    user: 'root',
-    password: ''
-});
-
+app.use(cors()); // Inicializa el middleware de CORS
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Cadena de conexión
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'm06',
+    user: 'userangular',
+    password: 'alumne123'
+});
+
+// Establece la conexión a la base de datos
+connection.connect(function (error) {
+    if (error) {
+        console.error("Error connecting: " + error.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+});
+
+
+
+
 app.listen(3000, () => {
-    console.log('Aquesta és la nostra API-REST que corre en http://localhost:3000');
+    console.log('API-REST en ejecución en http://localhost:3000');
+
+    app.post('/api/login', function (req, res) {
+        console.log("Estamos en login");
+
+
+        //recojo los valores enviados desde cliente
+        const usuari = req.body.user;
+        const contra = req.body.password;
+
+        console.log(usuari);
+        console.log(contra);
+
+        connection.query('SELECT * from users where username ="' + usuari + '"AND userpass = "' + contra + '"', function (error, results) {
+            if (error) {
+                res.status(400).send({results: null});
+            } else {
+                res.status(200).send({results: results});
+            }
+        });
+    });
+
+    // GET para realizar una consulta a la base de datos
+    app.get('/api/select/:userName', function (req, res) {
+        console.log("Estamos en login");
+
+
+        //recojo los valores enviados desde cliente
+        const usuari = req.params.userName;
+
+
+        console.log(usuari);
+
+        connection.query('SELECT username from users', function (error, results) {
+            if (error) {
+                res.status(400).send({results: null});
+            } else {
+                res.status(200).send({results: results});
+            }
+        });
+    });
+
 
     //només fem la petició get
-    app.get('/',(req,res)=>{
+    app.get('/', (req, res) => {
         res.send({
-            message:'Hola món'
+            message: 'Hola món'
         });
     })
+
 });
+
 
