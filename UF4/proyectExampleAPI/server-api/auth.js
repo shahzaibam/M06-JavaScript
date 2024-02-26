7611ecbb093931b7ce181c971994ede005aec03d
Shah Zaib <shebiraaja@gmail.com>
2024-02-26 11:25:37 +0100

    proyecto con api rest y token de login JWT

---
+++ UF4/proyectExampleAPI/server-api/auth.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Authentication service started on port 3000');
+});
+
+
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member'
+    }
+];
+
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+const accessTokenSecret = '$GFT653JHUS6$98JNS12@?8MMNGB';
+
+app.post('/login', (req, res) => {
+    // Read username and password from request body
+    const { username, password } = req.body;
+
+    // Filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // Generate an access token
+        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret, {expiresIn: '20m'});
+
+        res.json({
+            accessToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});
+
+
+
+
+
+
+const books = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "language": "English",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+    {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "language": "Danish",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+    },
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "language": "Italian",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    },
+];
+
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+app.get('/books', authenticateJWT, (req, res) => {
+    res.json(books);
+});
+
+
+
+//comprueba que esta logeado y que es rol admin, si lo es deja de logearlo
+app.post('/books', authenticateJWT, (req, res) => {
+    const { role } = req.user;
+
+    if (role !== 'admin') {
+        return res.sendStatus(403);
+    }
+
+
+    const book = req.body;
+    books.push(book);
+
+    res.send('Book added successfully');
+});
